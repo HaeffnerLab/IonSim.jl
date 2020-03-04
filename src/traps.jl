d6a2cf0381f5c6cbe81b54658054812faae0ecbf
Joseph Broz <jbroz@berkeley.edu>
2020-03-04 13:42:40 -0800

    fixed error in the way pi-times/ Rabi frequencies were calculated

--- src/traps.jl
+++ src/traps.jl
@@ -147,7 +147,7 @@ function Efield_from_pi_time(
     transition::Union{Tuple{String,String},Vector{<:String}}
 )
     (γ, ϕ) = map(x -> rad2deg(ndot(Bhat, x)), [laser.ϵ, laser.k])
-    s = laser.pointing[1][1]
+    s = laser.pointing[ion.number][2]
     Ω = s * ion.selected_matrix_elements[tuple(transition...)](1, γ, ϕ)
     1 / (Ω * 2 * pi_time) 
 end
