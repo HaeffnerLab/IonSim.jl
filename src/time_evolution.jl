f4cdbca2dd1ab5f245fd487edcfc876fc49c1b44
Joseph Broz <jbroz@berkeley.edu>
2020-04-10 23:31:59 -0700

    keywords passed on to master_dynamic

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -7,5 +7,6 @@ function timeevolution.schroedinger_dynamic(tspan, rho0::T, f::Function;
             fout::Union{Function,Nothing}=nothing, kwargs...
         ) where {B<:Basis,T<:DenseOperator{B,B}}
     Jvec = SparseOperator[]
-    timeevolution.master_dynamic(tspan, rho0, (t,rho) -> (f(t, rho), Jvec, Jvec))
+    timeevolution.master_dynamic(tspan, rho0, (t,rho) -> (f(t, rho), Jvec, Jvec), 
+        fout=fout, kwargs...)
 end
