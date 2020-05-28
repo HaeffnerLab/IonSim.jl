84f34570bff6a148ce537bb92c30904d3acaf33a
Joseph broz <josephbroz@MacBook-Pro.local>
2020-05-27 21:29:22 -0700

    small fix

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -5,7 +5,7 @@ import QuantumOptics.timeevolution
 # initial state that is pure and one that is mixed
 function timeevolution.schroedinger_dynamic(tspan, rho0::T, f::Function;
             fout::Union{Function,Nothing}=nothing, kwargs...
-        ) where {B<:Basis,T<:DenseOperator{B,B}}
+	   ) where {T<:Operator}
     Jvec = SparseOperator[]
     timeevolution.master_dynamic(tspan, rho0, (t,rho) -> (f(t, rho), Jvec, Jvec), 
         fout=fout, kwargs...)
