394b438c5d3ab1cd1571d79e8e0e619d2da12997
Joseph broz <josephbroz@MacBook-Pro.local>
2020-05-29 13:14:09 -0700

    small fix

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -1,12 +1,12 @@
 import QuantumOptics.timeevolution
-
+using QuantumOptics: Operator
 
 # when solving for unitary evolution, it is convenient to switch back and forth between an 
 # initial state that is pure and one that is mixed
 function timeevolution.schroedinger_dynamic(tspan, rho0::T, f::Function;
             fout::Union{Function,Nothing}=nothing, kwargs...
 	   ) where {T<:Operator}
-    Jvec = SparseOperator[]
+    Jvec = []
     timeevolution.master_dynamic(tspan, rho0, (t,rho) -> (f(t, rho), Jvec, Jvec), 
         fout=fout, kwargs...)
 end
