e5e2a56a10bc039bc812d19463313e3c65e1a058
thomasdellaert <thomasdellaert@physics.ucla.edu>
2022-05-24 12:55:53 -0700

    added basic citations for isotope shifts/transition frequencies, hyperfine splittings, and A coefficients

--- src/species/yb171.jl
+++ src/species/yb171.jl
@@ -28,26 +28,39 @@ const properties_yb171 = (
     charge = 1,
     nuclearspin = 1 // 2,
     full_level_structure = OrderedDict(
-        # should this be negative, or should I increase the energy of every state by 9GHz?
+        # Unless otherwise indicated, frequencies/isotope shifts are as used in the Campbell group,
+        #     and can be found in Conrad Roman's thesis (2020)
+        # Hyperfine splitting from Fisk et al (1997)
+        # Isotope shift from Martensson-Pendrill et al (1992)
         "S1/2f=0" => (n = 6, l = 0, j = 1 // 2, f = 0, E = -9.48210909315e9),
         "S1/2f=1" => (n = 6, l = 0, j = 1 // 2, f = 1, E = 3.16070303105e9),
+        # Hyperfine splitting from Engelke & Tamm (1996)
         "D3/2f=0" => (n = 5, l = 2, j = 3 // 2, f = 1, E = 6.88342460964640e14),
         "D3/2f=1" => (n = 5, l = 2, j = 3 // 2, f = 2, E = 6.88350470564640e14),
+        # Hyperfine splitting from Gill et al (1999)
         "D5/2f=2" => (n = 5, l = 2, j = 5 // 2, f = 2, E = 7.29477895985202e14),
         "D5/2f=3" => (n = 5, l = 2, j = 5 // 2, f = 3, E = 7.29474121985202e14),
+        # Hyperfine splitting from Martensson-Pendrill et al (1992)
+        # Isotope shift from Martensson-Pendrill et al (1992)
         "P1/2f=0" => (n = 6, l = 1, j = 1 // 2, f = 0, E = 8.112913749003559e14),
         "P1/2f=1" => (n = 6, l = 1, j = 1 // 2, f = 1, E = 8.112934798003559e14),
+        # Hyperfine splitting from Engelke & Tamm (1996)
         "[3/2]1/2f=0" => (n = 6, l = nothing, j = 1 // 2, f = 0, E = 1.008917341058788e15),
         "[3/2]1/2f=1" => (n = 6, l = nothing, j = 1 // 2, f = 1, E = 1.008917341058788e15),
+        # Hyperfine splitting can be found in Conrad Roman's thesis (2020)
         "[3/2]3/2f=1" => (n = 6, l = nothing, j = 3 // 2, f = 1, E = 8.621425511314839e14),
         "[3/2]3/2f=2" => (n = 6, l = nothing, j = 3 // 2, f = 2, E = 8.621425511314839e14),
+        # Hyperfine splitting from Feldker et al (2017)
         "[5/2]5/2f=2" => (n = 6, l = nothing, j = 5 // 2, f = 2, E = 9.70461163716380e14),
         "[5/2]5/2f=3" => (n = 6, l = nothing, j = 5 // 2, f = 3, E = 9.70461163716380e14),
+        # Hyperfine splitting from Gill et al (1999)
         "F7/2f=3" => (n = 6, l = 3, j = 7 // 2, f = 3, E = 6.42115934728750e14),
         "F7/2f=4" => (n = 6, l = 3, j = 7 // 2, f = 4, E = 6.42119554728750e14),
 
     ),
     full_transitions = Dict(
+        # Einstein A Coefficients sourced from the D.R.E.A.M. database at the Univ. of Mons
+        # (Quinet P., Palmeri P., Atoms 8(2), 18 (2020))
         # Laser lines
         # 411nm
         ("S1/2f=0", "D5/2f=2") => (multipole = "E2", einsteinA = 22),
@@ -84,10 +97,10 @@ const properties_yb171 = (
     ),
 
     # Optional fields
-    default_sublevel_selection = [("S1/2f=0", "all"), ("S1/2f=1", "all"), ("P1/2f=0", "all"), ("P1/2f=1", "all"),],
+    default_sublevel_selection = [("S1/2f=0", "all"), ("S1/2f=1", "all"), ("P1/2f=0", "all"), ("P1/2f=1", "all"), ],
     gfactors = Dict("S1/2f=0" => 1.998, "S1/2f=1" => 1.998, "D5/2f=2" => 1.202, "D5/2f=3" => 1.202, "F7/2f=3" => 1.145, "F7/2f=4" => 1.145,
     "[3/2]1/2f=0" => 1.32, "[3/2]1/2f=1" => 1.32, "[3/2]3/2f=1" => 1.44, "[3/2]3/2f=2" => 1.44,),
-    nonlinear_zeeman = Dict(("S1/2f=0", 0) => B->-155.305*B^2, ("S1/2f=1", 0) => B->155.305*B^2),
+    nonlinear_zeeman = Dict(("S1/2f=0", 0) => B->-155.305 * B^2, ("S1/2f=1", 0) => B->155.305 * B^2),
 )
 
 """
