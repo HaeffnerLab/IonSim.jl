f62578286228fb165e034233ad018d9218b53bc2
Neil Glikin <glikin@berkeley.edu>
2022-08-18 11:26:34 -0700

    Implement LightField abstract type and Microwave concrete type
    Types only defined so far; no changes to other code that uses Lasers yet. Laser and Microwave both now defined as subtypes of LightField. lasers.jl renamed to lightfields.jl.

--- src/lightfields.jl
+++ src/lightfields.jl
@@ -1,6 +1,20 @@
 using .PhysicalConstants: c
 
-export Laser
+export Laser, Microwave
+
+abstract type LightField end
+
+function Base.:(==)(LF1::LightField, LF2::LightField)
+    if typeof(LF1) != typeof(LF2)
+        return false
+    end
+    for field in fieldnames(typeof(LF1))
+        if getfield(LF1, field) != getfield(LF2, field)
+            return false
+        end
+    end
+    return true
+end
 
 """
     Laser(;λ=nothing, E=0, Δ=0, ϵ=(x̂+ŷ)/√2, k=ẑ, ϕ=0, pointing::Array{Tuple{Int,Real}})
@@ -20,7 +34,7 @@ The physical parameters defining laser light.
     (first element of the tuple is the index for an ion and the second element is the scaling
     factor for the laser's Efield which must be between 0 and 1).
 """
-mutable struct Laser
+mutable struct Laser <: LightField
     λ::Union{Real, Nothing}
     E::Function
     Δ::Real
@@ -56,15 +70,6 @@ mutable struct Laser
     end
     # for copying
     Laser(λ, E, Δ, ϵ, k, ϕ, pointing) = new(λ, E, Δ, ϵ, k, ϕ, pointing)
-end
-
-function Base.:(==)(L1::Laser, L2::Laser)
-    for field in fieldnames(Laser)
-        if getfield(L1, field) != getfield(L2, field)
-            return false
-        end
-    end
-    return true
 end
 
 function Base.print(L::Laser)
@@ -104,4 +109,78 @@ function Base.setproperty!(L::Laser, s::Symbol, v::Tv) where {Tv}
         return
     end
     return Core.setproperty!(L, s, v)
+end
+
+
+
+"""
+    Microwave(;λ=nothing, E=0, Δ=0, ϵ=(x̂+ŷ)/√2, k=ẑ, ϕ=0)
+        
+The physical parameters defining microwave radiation.
+**args**
+* `λ::Union{Real,Nothing}`: the wavelength of the microwave in meters
+* `E::Union{Function,Real}`: magnitude of the E-field in V/m
+* `Δ`: static detuning from f = c/λ in [Hz]
+* `ϵ::NamedTuple`: (ϵ.x, ϵ.y, ϵ.z), polarization direction, requires norm of 1
+* `k::NamedTuple`: (k.x, k.y, k.z), propagation direction, requires norm of 1
+* `ϕ::Union{Function,Real}`: time-dependent phase. of course, this can also be used to model a 
+    time-dependent detuning. Units are in radians. Note: if this is set to a function of time,
+    then when constructing a Hamiltonian with the `hamiltonian` function, the units of time
+    will be as specified by the `timescale` keyword argument.
+"""
+mutable struct Microwave <: LightField
+    λ::Union{Real, Nothing}
+    E::Function
+    Δ::Real
+    ϵ::NamedTuple{(:x, :y, :z)}
+    k::NamedTuple{(:x, :y, :z)}
+    ϕ::Function
+    function Microwave(;
+        λ = nothing,
+        E::TE = 0,
+        Δ = 0,
+        ϵ = (x̂ + ŷ) / √2,
+        k = ẑ,
+        ϕ::Tϕ = 0,
+    ) where {TE, Tϕ}
+        rtol = 1e-6
+        @assert isapprox(norm(ϵ), 1, rtol = rtol) "!(|ϵ| = 1)"
+        @assert isapprox(norm(k), 1, rtol = rtol) "!(|k| = 1)"
+        # @assert isapprox(ndot(ϵ, k), 0, rtol=rtol) "!(ϵ ⟂ k)"
+        # Above commented out until we figure out a better place to put this warning
+        TE <: Number ? Et(t) = E : Et = E
+        Tϕ <: Number ? ϕt(t) = ϕ : ϕt = ϕ
+        return new(λ, Et, Δ, ϵ, k, ϕt)
+    end
+    # for copying
+    Laser(λ, E, Δ, ϵ, k, ϕ) = new(λ, E, Δ, ϵ, k, ϕ)
+end
+
+function Base.print(MW::Microwave)
+    println("λ: ", MW.λ, " m")
+    println("Δ: ", MW.Δ, " Hz")
+    println("ϵ̂: ", "(x=$(MW.ϵ.x), y=$(MW.ϵ.y), z=$(MW.ϵ.z))")
+    println("k̂: ", "(z=$(MW.k.x), y=$(MW.k.y), z=$(MW.k.z))")
+    println("E(t=0): ", "$(MW.E(0.0)) V/m")
+    return println("ϕ(t=0): ", "$(MW.ϕ(0.0)) ⋅ 2π")
+end
+
+function Base.setproperty!(MW::Microwave, s::Symbol, v::Tv) where {Tv}
+    rtol = 1e-6
+    if s == :ϵ
+        @assert isapprox(norm(v), 1, rtol = rtol) "!(|ϵ| = 1)"
+        # if ! isapprox(ndot(L.k, v), 0, rtol=rtol)
+        #     @warn "!(ϵ ⟂ k)"
+        # end 
+    elseif s == :k
+        @assert isapprox(norm(v), 1, rtol = rtol) "!(|k| = 1)"
+        # if ! isapprox(ndot(v, L.ϵ), 0, rtol=rtol)
+        #     @warn "!(ϵ ⟂ k)"
+        # end 
+    elseif s == :E || s == :ϕ
+        Tv <: Number ? vt(t) = v : vt = v
+        Core.setproperty!(MW, s, vt)
+        return
+    end
+    return Core.setproperty!(MW, s, v)
 end
